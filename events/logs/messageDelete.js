1e5c6a38e6a79e66a35f5a17f3423e7ec36fc056
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 08:03:08 +0100

    Update messageDelete.js

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -1,19 +1,20 @@
-const axios = require('axios');         
+const axios = require('axios');
 const db = require("quick.db")
 const Discord = require("discord.js");
 const ms = require("ms")
 
-module.exports = (client,message) => {
-    let guild = message.guild
-   const color = db.get(`color_${guild.id}`) === null? client.config.color:db.get(`color_${guild.id}`)
+module.exports = (client, message) => {
+    
+	let guild = message.guild
+	const color = db.get(`color_${guild.id}`) === null ? client.config.color : db.get(`color_${guild.id}`)
+	let wass = db.get(`msglog_${message.guild.id}`);
+	const logschannel = message.guild.channels.cache.get(wass)
 
-          let wass = db.get(`msglog_${message.guild.id}`);
-          const logschannel = message.guild.channels.cache.get(wass)
-        
-          if(logschannel) logschannel.send(new Discord.MessageEmbed()
-          .setAuthor(`${message.author.username}` , `${message.author.displayAvatarURL({dynamic : true })}`)
-          .setColor(color)
-          .setDescription(`**Message supprimé dans** <#${message.channel.id}>\n ${message.content}`)
-          .setTimestamp())
-         
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+        .setColor(color)
+		.setAuthor(`Message supprimé`)
+		.setDescription(`Message supprimé dans <#${message.channel.id}> par ${message.author}`)
+		.addField(`Message Supprimé :`, `${message.content} ㅤ`)
+		.setFooter(`${client.config.name}`)
+		.setTimestamp())
 }
