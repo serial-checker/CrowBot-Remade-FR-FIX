65f2c8b23ba018a0a8c4b7442a5a007bb4c577b1
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 09:08:51 +0100

    Update start.js

--- util/embedButton/start.js
+++ util/embedButton/start.js
@@ -1,51 +1,67 @@
-const { MessageActionRow, MessageButton } = require('discord-buttons');
+const {
+	MessageActionRow,
+	MessageButton
+} = require('discord-buttons');
 const db = require("quick.db");
-const buttonInteraction = async function (button, interaction) {
-    if (interaction.interactor !== button.clicker.user || button.message.id !== interaction.message.id) return ;
-    if (button.id == 'next-page') {
-        (interaction.currentPage + 1 == interaction.embeds.length ? interaction.currentPage = 0 : interaction.currentPage += 1);
-        interaction.message.edit({ embed: interaction.embeds[interaction.currentPage], components: [interaction.components] });
-        button.reply.defer(true);
-    } else if (button.id == 'back-page') {
-        (interaction.currentPage - 1 < 0 ? interaction.currentPage = interaction.embeds.length - 1 : interaction.currentPage -= 1);
-        interaction.message.edit({ embed: interaction.embeds[interaction.currentPage], components: [interaction.components] });
-        button.reply.defer(true);
-    } 
-    }
-     
+
+const buttonInteraction = async function(button, interaction) {
+	if (interaction.interactor !== button.clicker.user || button.message.id !== interaction.message.id) return;
+	if (button.id == 'next-page') {
+		(interaction.currentPage + 1 == interaction.embeds.length ? interaction.currentPage = 0 : interaction.currentPage += 1);
+		interaction.message.edit({
+			embed: interaction.embeds[interaction.currentPage],
+			components: [interaction.components]
+		});
+		button.reply.defer(true);
+	} else if (button.id == 'back-page') {
+		(interaction.currentPage - 1 < 0 ? interaction.currentPage = interaction.embeds.length - 1 : interaction.currentPage -= 1);
+		interaction.message.edit({
+			embed: interaction.embeds[interaction.currentPage],
+			components: [interaction.components]
+		});
+		button.reply.defer(true);
+	}
+}
 
 
-const ButtonPages = async function (interaction, message, embeds, duration, buttonStyle, rightEmoji, leftEmoji) {
 
-    if (!['red', 'green', 'blurple', "gray"].includes(buttonStyle)) throw new TypeError(`Button style incorect`);
-    if (!rightEmoji) throw new TypeError(`Emoji pour le 1er boutous n'est pas fournis`);
-    if (!leftEmoji) throw new TypeError(`Emoji pour le 2eme boutous n'est pas fournis`);
+const ButtonPages = async function(interaction, message, embeds, duration, buttonStyle, rightEmoji, leftEmoji) {
 
-    const button1 = new MessageButton()
-        .setLabel(rightEmoji)
-        .setStyle(buttonStyle)
-        .setID('next-page');
+	if (!['red', 'green', 'blurple', "gray"].includes(buttonStyle)) throw new TypeError(`Button style incorect`);
+	if (!rightEmoji) throw new TypeError(`Emoji pour le 1er boutous n'est pas fournis`);
+	if (!leftEmoji) throw new TypeError(`Emoji pour le 2eme boutous n'est pas fournis`);
 
-    const button2 = new MessageButton()
-        .setLabel(leftEmoji)
-        .setStyle(buttonStyle)
-        .setID('back-page');
+	const button1 = new MessageButton()
+		.setLabel(rightEmoji)
+		.setStyle(buttonStyle)
+		.setID('next-page');
 
-    const interactiveButtons = new MessageActionRow()
-        .addComponent(button2)
-        .addComponent(button1);
+	const button2 = new MessageButton()
+		.setLabel(leftEmoji)
+		.setStyle(buttonStyle)
+		.setID('back-page');
 
-    await message.channel.send({ components: [interactiveButtons], embed: embeds[0] }).then((m) => {
-        interaction.message = m;
-        interaction.embeds = embeds;
-        interaction.currentPage = 0;
-        interaction.interactor = message.author;
-        interaction.components = interactiveButtons;
-        setTimeout(() => {
-            m.edit("", { components: [], embed: embeds[interaction.currentPage] })
-        }, 60000 * 5)
+	const interactiveButtons = new MessageActionRow()
+		.addComponent(button2)
+		.addComponent(button1);
 
-    })
+	await message.channel.send({
+		components: [interactiveButtons],
+		embed: embeds[0]
+	}).then((m) => {
+		interaction.message = m;
+		interaction.embeds = embeds;
+		interaction.currentPage = 0;
+		interaction.interactor = message.author;
+		interaction.components = interactiveButtons;
+		setTimeout(() => {
+			m.edit("", {
+				components: [],
+				embed: embeds[interaction.currentPage]
+			})
+		}, 60000 * 5)
+
+	})
 
 
 }
@@ -53,11 +69,6 @@ const ButtonPages = async function (interaction, message, embeds, duration, butt
 
 
 module.exports = {
-    ButtonPages,
-    buttonInteraction
+	ButtonPages,
+	buttonInteraction
 }
-
-
-
-
-
