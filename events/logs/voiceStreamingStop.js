1a81806e4bb49d461235acfafdad4895f647bb81
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 08:06:36 +0100

    Update voiceStreamingStop.js

--- events/logs/voiceStreamingStop.js
+++ events/logs/voiceStreamingStop.js
@@ -4,12 +4,15 @@ const Discord = require("discord.js");
 const ms = require("ms")
 
 module.exports = (client, member, voiceChannel) => {
- const color = db.get(`color_${member.guild.id}`) === null ? client.config.prefix : db.get(`color_${member.guild.id}`)
-  let wass = db.get(`logvc_${voiceChannel.guild.id}`);
-  const logschannel = voiceChannel.guild.channels.cache.get(wass)
-  if (logschannel) logschannel.send(new Discord.MessageEmbed()
-    .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
-    .setColor(color)
-    .setDescription(`**${member}** ne partage plus son écran dans ${voiceChannel.name}`)
-    .setTimestamp())
+    
+	const color = db.get(`color_${member.guild.id}`) === null ? client.config.color : db.get(`color_${member.guild.id}`)
+	let wass = db.get(`logvc_${voiceChannel.guild.id}`);
+	const logschannel = voiceChannel.guild.channels.cache.get(wass)
+    
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+		.setColor(color)
+		.setDescription(`${member} **Ne partage plus sont stream** dans <#${voiceChannel.id}>`)
+		.setFooter(`${client.config.name}`)
+		.setTimestamp()
+	)
 }
