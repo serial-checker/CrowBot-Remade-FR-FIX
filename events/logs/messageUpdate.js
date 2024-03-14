901810b403ce6c7d1d92b86ca327b0b46e5f6adb
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 08:04:02 +0100

    Update messageUpdate.js

--- events/logs/messageUpdate.js
+++ events/logs/messageUpdate.js
@@ -1,22 +1,21 @@
-const axios = require('axios');         
+const axios = require('axios');
 const db = require("quick.db")
 const Discord = require("discord.js");
 const ms = require("ms")
 
 module.exports = async (client, oldMessage, newMessage) => {
 
-    let guild = oldMessage.guild
-   const color = db.get(`color_${guild.id}`) === null? client.config.color:db.get(`color_${guild.id}`)
-
-    let wass = db.get(`msglog_${oldMessage.guild.id}`);
-  
-    const logschannel = oldMessage.guild.channels.cache.get(wass)
-  
-    if(logschannel) logschannel.send(new Discord.MessageEmbed()
-    .setColor(color)
-    .setAuthor(`${oldMessage.author.username}` , `${oldMessage.author.displayAvatarURL({dynamic : true })}`)
-    .setDescription(`**Message édité dans** <#${oldMessage.channel.id}>` )
-    .addField(`Avant` , `${oldMessage.content}`)
-    .addField(`Aprés` , `${newMessage.content}`)
-    .setTimestamp())
+	let guild = oldMessage.guild
+	const color = db.get(`color_${guild.id}`) === null ? client.config.color : db.get(`color_${guild.id}`)
+	let wass = db.get(`msglog_${oldMessage.guild.id}`);
+	const logschannel = oldMessage.guild.channels.cache.get(wass)
+    
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+		.setColor(color)
+		.setAuthor(`Message modifié`)
+		.setDescription(`${oldMessage.author} À modifié sont message dans <#${oldMessage.channel.id}>`)
+		.addField(`Ancien message :`, `${oldMessage.content}ㅤ`)
+		.addField(`Nouveau message :`, `${newMessage.content}ㅤ`)
+		.setFooter(`${client.config.name}`)
+		.setTimestamp())
 }
