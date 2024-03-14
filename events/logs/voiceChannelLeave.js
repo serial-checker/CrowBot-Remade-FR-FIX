f749a1bb2aab254580bb7de19432c87090d55c3a
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 08:04:49 +0100

    Update voiceChannelLeave.js

--- events/logs/voiceChannelLeave.js
+++ events/logs/voiceChannelLeave.js
@@ -1,22 +1,19 @@
-const axios = require('axios');         
+const axios = require('axios');
 const db = require("quick.db")
 const Discord = require("discord.js");
 const ms = require("ms")
 
-
-module.exports = async (client, member, voiceChannel) => {
-   const color = db.get(`color_${member.guild.id}`) === null? client.config.color:db.get(`color_${member.guild.id}`)
-
-        let wass = db.get(`logvc_${member.guild.id}`);
-      
-  
-        const logschannel = member.guild.channels.cache.get(wass)
-      
-        if(logschannel) logschannel.send(new Discord.MessageEmbed()
-        .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic : true }))
-        .setColor(color)
-        .setDescription(`**${member}** quitte le salon ${channel.name}`)
-        )
+module.exports = async (client, member, channel) => {
     
+	const color = db.get(`color_${member.guild.id}`) === null ? client.config.color : db.get(`color_${member.guild.id}`)
+	let wass = db.get(`logvc_${member.guild.id}`);
+	const logschannel = member.guild.channels.cache.get(wass)
+    
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+
+		.setColor(color)
+		.setDescription(`${member} Quitte le **salon vocal** <#${channel.id}>`)
+		.setFooter(`${client.config.name}`)
+		.setTimestamp()
+	)
 }
-  
