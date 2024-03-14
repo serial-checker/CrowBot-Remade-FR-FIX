8a6a316907299b3d46080cc052a6eb58be2e620c
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-14 08:06:01 +0100

    Update voiceStreamingStart.js

--- events/logs/voiceStreamingStart.js
+++ events/logs/voiceStreamingStart.js
@@ -4,11 +4,15 @@ const Discord = require("discord.js");
 const ms = require("ms")
 
 module.exports = (client, member, voiceChannel) => {
- const color = db.get(`color_${member.guild.id}`) === null ? client.config.prefix : db.get(`color_${member.guild.id}`)
-  let wass = db.get(`logvc_${voiceChannel.guild.id}`);
-  const logschannel = voiceChannel.guild.channels.cache.get(wass)
-  if (logschannel) logschannel.send(new Discord.MessageEmbed()
-    .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
-    .setColor(color)
-    .setDescription(`**${member}** partage son écran dans ${voiceChannel.name}`))
+    
+	const color = db.get(`color_${member.guild.id}`) === null ? client.config.color : db.get(`color_${member.guild.id}`)
+	let wass = db.get(`logvc_${voiceChannel.guild.id}`);
+	const logschannel = voiceChannel.guild.channels.cache.get(wass)
+    
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+		.setColor(color)
+		.setDescription(`${member} **Partage sont stream** dans <#${voiceChannel.id}>`)
+		.setFooter(`${client.config.name}`)
+		.setTimestamp()
+	)
 }
