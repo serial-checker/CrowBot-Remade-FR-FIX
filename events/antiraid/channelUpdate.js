a42fc84af740743d5149adc49ab545dd007e80cc
Sérial <142848494+Serial-Checker@users.noreply.github.com>
2024-03-16 14:08:27 +0100

    Update channelUpdate.js

--- events/antiraid/channelUpdate.js
+++ events/antiraid/channelUpdate.js
@@ -5,157 +5,79 @@ const {
 } = require("discord.js");
 const ms = require("ms")
 
-module.exports = (client, channel) => {
-
-	const guild = channel.guild
+module.exports = async (client, oldChannel, newChannel) => {
+	const guild = oldChannel.guild
 	const color = db.get(`color_${guild.id}`) === null ? client.config.color : db.get(`color_${guild.id}`)
+	const raidlog = guild.channels.cache.get(db.get(`${guild.id}.raidlog`))
 
-	try {
+	axios.get(`https://discord.com/api/v9/guilds/${oldChannel.guild.id}/audit-logs?ilimit=1&action_type=11`, {
+		headers: {
+			Authorization: `Bot ${client.config.token}`
+		}
+	}).then(response => {
+		if (response.data && response.data.audit_log_entries[0].user_id) {
+			let perm = ""
+			if (db.get(`channelsmodwl_${guild.id}`) === null) perm = client.user.id === response.data.audit_log_entries[0].user_id || guild.owner.id === response.data.audit_log_entries[0].user_id || client.config.owner.includes(response.data.audit_log_entries[0].user_id) || db.get(`ownermd_${client.user.id}_${response.data.audit_log_entries[0].user_id}`) === true || db.get(`wlmd_${guild.id}_${response.data.audit_log_entries[0].user_id}`) === true
+			if (db.get(`channelsmodwl_${guild.id}`) === true) perm = client.user.id === response.data.audit_log_entries[0].user_id || guild.owner.id === response.data.audit_log_entries[0].user_id || client.config.owner.includes(response.data.audit_log_entries[0].user_id) || db.get(`ownermd_${client.user.id}_${response.data.audit_log_entries[0].user_id}`) === true
+			if (db.get(`channelsmod_${guild.id}`) === true && !perm) {
+				if (db.get(`channelsmodsanction_${guild.id}`) === "ban") {
+					axios({
+						url: `https://discord.com/api/v9/guilds/${guild.id}/bans/${response.data.audit_log_entries[0].user_id}`,
+						method: 'PUT',
+						headers: {
+							Authorization: `bot ${client.config.token}`
+						},
+						data: {
+							delete_message_days: '1',
+							reason: 'Antichannel'
+						}
+					}).then(() => {
 
-		// -- Audit Logs
-		axios.get(`https://discord.com/api/v9/guilds/${guild.id}/audit-logs?ilimit=1&action_type=12`, {
-			headers: {
-				Authorization: `Bot ${client.config.token}`
-			}
-		}).then(response => {
-			if (response.data && response.data.audit_log_entries[0].user_id) {
-				let perm = ""
-				if (db.get(`channelsdelwl_${guild.id}`) === null) perm = client.user.id === response.data.audit_log_entries[0].user_id || guild.owner.id === response.data.audit_log_entries[0].user_id || client.config.owner.includes(response.data.audit_log_entries[0].user_id) || db.get(`ownermd_${client.user.id}_${response.data.audit_log_entries[0].user_id}`) === true || db.get(`wlmd_${guild.id}_${response.data.audit_log_entries[0].user_id}`) === true
-				if (db.get(`channelsdelwl_${guild.id}`) === true) perm = client.user.id === response.data.audit_log_entries[0].user_id || guild.owner.id === response.data.audit_log_entries[0].user_id || client.config.owner.includes(response.data.audit_log_entries[0].user_id) || db.get(`ownermd_${client.user.id}_${response.data.audit_log_entries[0].user_id}`) === true
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, il a été **ban** !`))
+					}).catch(() => {
 
-				if (db.get(`channelsdel_${guild.id}`) === true && !perm) {
-					const raidlog = guild.channels.cache.get(db.get(`${guild.id}.raidlog`))
-					if (db.get(`channelsdelsanction_${guild.id}`) === "ban") {
-						axios({
-							url: `https://discord.com/api/v9/guilds/${guild.id}/bans/${response.data.audit_log_entries[0].user_id}`,
-							method: 'PUT',
-							headers: {
-								Authorization: `Bot ${client.config.token}`
-							},
-							data: {
-								delete_message_days: '1',
-								reason: 'Antichannel'
-							}
-						}).then(() => {
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, il a été **ban** !`))
-						}).catch(() => {
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, mais il n'a pas pu être **ban** !`))
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, mais il n'a pas pu être **ban** !`))
 
-						})
-					} else if (db.get(`channelsdelsanction_${guild.id}`) === "kick") {
-						guild.members.cache.get(response.data.audit_log_entries[0].user_id).kick().then(() => {
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, il a été **kick** !`))
-						}).catch(() => {
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, mais il n'a pas pu être **kick** !`))
-						})
-					} else if (db.get(`channelsdelsanction_${guild.id}`) === "derank") {
+					})
+				} else if (db.get(`channelsmodsanction_${guild.id}`) === "kick") {
+					guild.members.cache.get(response.data.audit_log_entries[0].user_id).kick().then(() => {
 
-						guild.members.cache.get(response.data.audit_log_entries[0].user_id).roles.set([]).then(() => {
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, il a été **kick** !`))
+					}).catch(() => {
 
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, il a été **derank** !`))
-						}).catch(() => {
-							channel.clone({
-								name: channel.name,
-								permissions: channel.permissionsOverwrites,
-								type: channel.type,
-								//  parent: channel.parent,
-								topic: channel.withTopic,
-								nsfw: channel.nsfw,
-								birate: channel.bitrate,
-								userLimit: channel.userLimit,
-								rateLimitPerUser: channel.rateLimitPerUser,
-								permissions: channel.withPermissions,
-								position: channel.rawPosition,
-								reason: `Antichannel`
-							})
-							if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a supprimé le salon \`${channel.name}\`, mais il n'a pas pu être **derank** !`))
-						})
-					}
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, mais il n'a pas pu être **kick** !`))
+					})
+				} else if (db.get(`channelsmodsanction_${guild.id}`) === "derank") {
+
+					guild.members.cache.get(response.data.audit_log_entries[0].user_id).roles.set([]).then(() => {
 
 
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, il a été **derank** !`))
+					}).catch(() => {
 
+						if (raidlog) return raidlog.send(new MessageEmbed().setColor(color).setDescription(`<@${response.data.audit_log_entries[0].user_id}> a modifier le salon ${oldChannel}, mais il n'a pas pu être **derank** !`))
+					})
 				}
 
-
+				newChannel.edit({
+					name: oldChannel.name,
+					permissions: oldChannel.permissionsOverwrites,
+					type: oldChannel.type,
+					topic: oldChannel.withTopic,
+					nsfw: oldChannel.nsfw,
+					birate: oldChannel.bitrate,
+					userLimit: oldChannel.userLimit,
+					rateLimitPerUser: oldChannel.rateLimitPerUser,
+					position: oldChannel.rawPosition,
+					reason: `Antichannel`
+				})
+				newChannel.overwritePermissions(oldChannel.permissionOverwrites)
 			}
 
+		}
 
-		});
-
-	} catch (error) {
-		return
-	}
+	});
 
 
 
-};
+}
