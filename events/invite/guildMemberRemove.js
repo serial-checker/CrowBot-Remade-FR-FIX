7b890717b8854221a0bd8295fbbc88a4b9cf6858
serial_checker <142848494+Serial-Checker@users.noreply.github.com>
2024-06-09 05:12:34 +0200

    Update guildMemberRemove.js

--- events/invite/guildMemberRemove.js
+++ events/invite/guildMemberRemove.js
@@ -15,7 +15,7 @@ module.exports = async (client, member) => {
 		if (member.user.bot && welcomeChannel) return welcomeChannel.send(new MessageEmbed().setColor(color).setDescription(`Le bot ${member.toString()} nous a quitter, il avait rejoins en utilisant ***l'api OAuth2***`)).catch(err => {});
 		let user = db.get(`inviter_${member.guild.id}_${member.id}`);
 		if (user === null && welcomeChannel) return welcomeChannel.send(new MessageEmbed().setColor(color).setDescription(`**${member.user.tag}** a quitter le serveur, mais **je n'arrive pas à trouver** comment il l'avait rejoint.`));
-		if (user === 'vanity' && welcomeChannel) return welcomeChannel.send(new MessageEmbed().setColor(color).setDescription(`**${member.user.tag}** nous a quitter il avais rejoint en utilisant ***le lien d'invitation personnalisé du serveur.**`));
+		if (user === 'vanity' && welcomeChannel) return welcomeChannel.send(new MessageEmbed().setColor(color).setDescription(`**${member.user.tag}** nous a quitter il avais rejoint en utilisant ***le lien d'invitation personnalisé du serveur.***`));
 		let iv2 = await client.users.fetch(user).catch(err => {});
 		let inv = db.fetch(`invites_${member.guild.id}_${user}`);
 		if (inv == null) inv = 0
