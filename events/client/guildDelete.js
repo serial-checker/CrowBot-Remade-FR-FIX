0431541698f8b2acfc12aa5c239679fdebf3cd0e
serial_checker <142848494+Serial-Checker@users.noreply.github.com>
2024-06-09 05:14:53 +0200

    Update guildDelete.js

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -8,11 +8,11 @@ const ms = require("ms")
 module.exports = async (client, guild) => {
 
 	console.log(`J'ai quitter le serveur ${guild.name} [${guild.memberCount}]`)
-	client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de quitter ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`))
+	client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de quitter le serveur : ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`))
 
 	let own = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data)
 	own.filter(x => client.users.cache.get(x.ID.split('_')[2])).map((m, i) => {
-		client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de quitter ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`)
+		client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de quitter le serveur : ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`)
 	})
 
 }
