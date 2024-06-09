50bdbc40fb32cd4c81d2dff9e1fc3c6d205fb41e
serial_checker <142848494+Serial-Checker@users.noreply.github.com>
2024-06-09 05:14:10 +0200

    Update guildCreate.js

--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -8,18 +8,18 @@ const ms = require("ms")
 module.exports = async (client, guild) => {
 	console.log(`J'ai rejoint le serveur ${guild.name} [${guild.memberCount}]`)
 	if (db.get(`antijoinbot_${client.user.id}`) === null) {
-		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`))
+		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre le serveur :  ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`))
 		let own = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data)
 		own.filter(x => client.users.cache.get(x.ID.split('_')[2])).map((m, i) => {
-			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`)
+			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre le serveur : ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>)`)
 		})
 
 	} else if (db.get(`antijoinbot_${client.user.id}`) === true) {
 		let own = db.all().filter(data => data.ID.startsWith(`ownermd_${client.user.id}`)).sort((a, b) => b.data - a.data)
-		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`))
+		client.config.owner.forEach(u => client.users.cache.get(u).send(`Je viens de rejoindre le serveur : ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`))
 
 		own.filter(x => client.users.cache.get(x.ID.split('_')[2])).map((m, i) => {
-			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`)
+			client.users.cache.get(m.ID.split('_')[2]).send(`Je viens de rejoindre le serveur : ${guild.name} (${guild.memberCount} membres, propriétaire : <@${guild.owner.id}>) mais j'ai quitter car l'antijoin du bot est activé (\`botconfig\`)`)
 		})
 		return guild.leave()
 	}
