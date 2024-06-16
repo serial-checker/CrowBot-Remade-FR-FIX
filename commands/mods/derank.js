fae4505d37b66328acfdad946c39af083cd9f7a1
Sérial357 aka 100animosité <142848494+Serial0357@users.noreply.github.com>
2024-06-16 12:10:00 +0200

    Update derank.js

--- commands/mods/derank.js
+++ commands/mods/derank.js
@@ -30,19 +30,17 @@ module.exports = {
                 if (db.get(`ownermd_${message.author.id}`) === true) return message.channel.send(`Vous n'avez pas la permission de **derank** <@${user.id}>`);
                 if (client.config.owner.includes(user.id)) return message.channel.send(`Vous n'avez pas la permission de **derank** *(vous ne pouvez pas derank un owner)* <@${user.id}>`);
 
-
-
-
 					message.channel.send(`${user} à été **derank**`)
 					user.roles.set([], `Derank par ${message.author.tag}`).catch(err => {})
 					let wass = db.get(`logmod_${message.guild.id}`);
 					const logschannel = message.guild.channels.cache.get(wass)
+				
 					if (logschannel) logschannel.send(new Discord.MessageEmbed()
-						//         .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+					      //.setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
 						.setColor(color)
-						//       .setTitle(`<:protection:847072581382438953> Modération • Type: **\`derank\`**`)
-						//     .setTimestamp() 
-						//   .setDescription(`**Derank de**: ${user}\n**Auteur**: ${message.author}\n**Temps de réponse**: ${client.ws.ping}ms`)
+					      //.setTitle(`<:protection:847072581382438953> Modération • Type: **\`derank\`**`)
+					      //.setTimestamp() 
+					      //.setDescription(`**Derank de**: ${user}\n**Auteur**: ${message.author}\n**Temps de réponse**: ${client.ws.ping}ms`)
 						.setDescription(`${message.author} à **derank** ${user.user}`)
 					)
 				}
