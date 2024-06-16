4fb2b15a026d9122748b96466dda58d88bedcb56
Sérial357 aka 100animosité <142848494+Serial0357@users.noreply.github.com>
2024-06-16 02:38:57 +0200

    Update serverinfo.js

--- commands/utilitaire/serverinfo.js
+++ commands/utilitaire/serverinfo.js
@@ -24,7 +24,7 @@ module.exports = {
 			const guild = client.guilds.cache.get(args[0]) || message.guild
 
 			let boosters = message.guild.members.cache.filter(m => m.premiumSince)?.map(function(m) {return `<@${m.id}> Boost Depuis : <t:${parseInt(m.premiumSinceTimestamp / 1000)}:f>`}).join("\n");
-			if(!boosters || boosters.length < 1) boosters = "__Personne n'est en train de booster le serveur__";
+			if(!boosters || boosters.length < 1) boosters = "__**Personne n'est en train de booster le serveur**__";
 
 			const verificationLevels = {
 				NONE: '0',
@@ -41,22 +41,22 @@ module.exports = {
 			//console.log(NoRoles)
 			const ServerInfo = new Discord.MessageEmbed()
 				.setTitle(`${guild.name} `)
-				.setURL("https://discord.gg/9ZfB8m5E88")
-				.addField(`Identifiant Serveur <a:serial_warning:1220471714056507535> :`, `__${guild.id}__`, true)
-				.addField(`Niveau De Vérification :`, `__${verificationLevels[message.guild.verificationLevel]}__`, true)
-				.addField(`Nombre De Membre(s) <:serial_membre:1217042139037438032> :`, `__${guild.memberCount}__`, true)
-				.addField(`Nombre De Membre(s) Actif(s) <a:serial_terre:1220445710223151217> :`, `__${guild.members.cache.filter(m => m.presence?.status === 'online' || m.presence?.status === 'dnd' || m.presence?.status === 'streaming' || m.presence?.status === 'idle').size}__`, true)
-				.addField(`Nombre D'humain(s) <:serial_membre:1220474644004339713> :`, `__${guild.members.cache.filter((m) => !m.user.bot).size}__`, true)
-				.addField(`Nombre De Bot(s) :robot: :`, `__${guild.members.cache.filter((m) => m.user.bot).size}__`, true)
-				.addField(`Nombre D'utilisateur(s) En Vocal <:serial_vocal:1217007411068669963> :`, `__${guild.members.cache.filter(m => m.voice.channel).size}__`, true)
-				.addField(`Nombre D'utilisateur(s) Sans Rôle :x: :`, `__${NoRoles}__`, true)
-				.addField(`Nombre De Boost(s) <a:serial_boost:1217008346662699098> :`, `__${guild.premiumSubscriptionCount}__`, true)
-				//.addField(`Nombre De Booster(s) <a:serial_nitroboost:1217195480052666378> :`, `${boosters}`, true)
-				.addField(`Nombre De Rôle(s) :scroll: :`, `__${guild.roles.cache.size}__`, true)
-				.addField(`Nombre De Salon(s) <a:serial_chat:1220445576467058739> :`, `__${guild.channels.cache.size}__`, true)
-				.addField(`Nombre D'émoji(s) Total :package: :`, `__${guild.emojis.cache.size}__`, true)
+				.setURL("https://discord.gg/2BvuJ9mnFd")
+				.addField(`Identifiant Serveur:`, `__**${guild.id}**__`, true)
+				.addField(`Niveau de vérification:`, `__**${verificationLevels[message.guild.verificationLevel]}**__`, true)
+				.addField(`Nombre de membre(s):`, `__**${guild.memberCount}**__`, true)
+				.addField(`Nombre de membre(s) Actif(s):`, `__**${guild.members.cache.filter(m => m.presence?.status === 'online' || m.presence?.status === 'dnd' || m.presence?.status === 'streaming' || m.presence?.status === 'idle').size}**__`, true)
+				.addField(`Nombre d'humain(s):`, `__**${guild.members.cache.filter((m) => !m.user.bot).size}**__`, true)
+				.addField(`Nombre de bot(s):`, `__**${guild.members.cache.filter((m) => m.user.bot).size}**__`, true)
+				.addField(`Nombre d'utilisateur(s) en vocal:`, `__**${guild.members.cache.filter(m => m.voice.channel).size}**__`, true)
+				.addField(`Nombre d'utilisateur(s) sans rôle:`, `__**${NoRoles}**__`, true)
+				.addField(`Nombre de boost(s):`, `__**${guild.premiumSubscriptionCount}**__`, true)
+				//.addField(`Nombre de booster(s):`, `__**${boosters}**__`, true)
+				.addField(`Nombre de rôle(s):`, `__**${guild.roles.cache.size}**__`, true)
+				.addField(`Nombre de salon(s):`, `__**${guild.channels.cache.size}**__`, true)
+				.addField(`Nombre d'émoji(s) total:`, `__**${guild.emojis.cache.size}**__`, true)
 				.setImage(message.guild.bannerURL({size: 1024}))
-				.setFooter("La Création Du Serveur Remonte au :")
+				.setFooter("La création du serveur remonte au")
 				.setTimestamp(guild.createdAt)
 
 				.setThumbnail(guild.iconURL({
