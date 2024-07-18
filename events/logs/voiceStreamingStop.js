9afba7e50d9e63868f4970b6c1e8993ec3b2d6cb
Sérial357 <142848494+serial-checker@users.noreply.github.com>
2024-07-19 01:47:12 +0200

    Update voiceStreamingStop.js

--- events/logs/voiceStreamingStop.js
+++ events/logs/voiceStreamingStop.js
@@ -12,7 +12,7 @@ module.exports = (client, member, voiceChannel) => {
 	if (logschannel) logschannel.send(new Discord.MessageEmbed()
 		.setColor(color)
 		.setDescription(`${member} a **arrété son partage** dans <#${voiceChannel.id}>`)
-		.setFooter(`${client.config.name}`)
+		//.setFooter(`${client.config.name}`)
 		.setTimestamp()
 	)
 }
