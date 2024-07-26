c4a44cc81900c5809152c77c9717158dfb541a76
Sérial357 <142848494+serial-checker@users.noreply.github.com>
2024-07-26 22:15:16 +0200

    Update channelDelete.js

--- events/antiraid/channelDelete.js
+++ events/antiraid/channelDelete.js
@@ -34,7 +34,7 @@ module.exports = (client, channel) => {
 							},
 							data: {
 								delete_message_days: '1',
-								reason: 'Antichannel'
+								reason: 'AntiChannel Delete'
 							}
 						}).then(() => {
 							channel.clone({
